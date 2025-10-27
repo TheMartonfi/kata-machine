8b07e94e2a7d8982a5e697944448b60a7afaeeb3
TheMartonfi <carlmartonfi@hotmail.com>
2025-10-27 17:29:43 -0600

    Compare Binary Trees Implementation

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,29 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b === null) {
+        return true;
+    }
 
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // My implementation
+    // if (JSON.stringify(a) === JSON.stringify(b)) {
+    //     return true;
+    // }
+    //
+    // compare(a?.left, b?.left);
+    // compare(a?.right, b?.right);
+    //
+    // return false;
+
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a?.left, b?.left) && compare(a?.right, b?.right);
 }
+
